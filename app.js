fa6e57bbfb7faca809dadd74aef20bee5c20ddad
Pratush <pratushtiwari01@gmail.com>
2021-10-13 22:08:52 +0530

    video added

--- app.js
+++ app.js
@@ -19,3 +19,51 @@ tl.fromTo(hero, 1, { height: "0%"}, { height: "80%", ease: Power2.easeInOut })
     { x: "-100%" }, 
     { x: "0%", ease: Power2.easeInOut }, 
     "-=1.2")
+
+
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 1000,
+  triggerElement:section,
+  triggerHook: 0.5
+})
+  .addIndicators()
+
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 150;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = scrollpos;
+}, 33.3);
